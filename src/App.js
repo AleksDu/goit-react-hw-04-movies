0b38ddf1a1d0800766b29622a33127be17fd485a
AleksDu <aleksandr.dyshkin@gmail.com>
2021-10-29 16:29:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   return (
+    <h1>Hello</h1>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
